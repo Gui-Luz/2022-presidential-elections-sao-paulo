4ab245ecf83bbb1cfb877cad2be01a50849837a3
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2023-01-17 14:40:04 -0300

    change in function

--- helpers/calculateGraphValues.js
+++ helpers/calculateGraphValues.js
@@ -7,7 +7,7 @@ function calculateGraphValues(x1,y1,x2,y2) {
     v1 = x1 - x2;
     v2 = y1 - y2;
 
-    if (v1 > v2) {
+    if (v1 < v2) {
         return [x2, v1, 0, y1];
     }else{
         return [x1, 0, v2, y2];
