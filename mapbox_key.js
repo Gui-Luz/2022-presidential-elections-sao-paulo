0fcb9c24cf7dae45591a23fbd1fce70712e251d5
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2023-01-17 14:11:43 -0300

    change key

--- mapbox_key.js
+++ mapbox_key.js
@@ -1,1 +1,1 @@
-const mapbox_key = "pk.eyJ1IjoiZ3Vpc2FsIiwiYSI6ImNsYjg3b3UzdDBoaWQzcG8yb2ZzZHAwOXEifQ.-53yFkBrBYgkfE6KcT--Dg"
+const mapbox_key = "ppk.eyJ1IjoiZ3Vpc2FsIiwiYSI6ImNsZDBocnUzcjI3azQzd3JsdHJhYmY0MWMifQ.f4wXsehl5aPt2m1pS383EA"
